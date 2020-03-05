7d2f39d2123cbcc422b3fb9eece9769bfe9b65f4
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2020-03-05 14:10:49 -0300

    Adding the create invite shortcode

--- all-in-one-invite-codes-buddypress.php
+++ all-in-one-invite-codes-buddypress.php
@@ -59,11 +59,25 @@ function all_in_one_invite_codes_profile_tab_tab_title() {
 
 
 function all_in_one_invite_codes_profile_tab_tab_content() {
+    global $post;
+    $post_id = '';
 	$args = array(
 		'author'         => get_current_user_id(),
 		'posts_per_page' => - 1,
 		'post_type'      => 'tk_invite_codes', //you can use also 'any'
 	);
+	AllinOneInviteCodes::setNeedAssets(true, 'buddypress');
+	?>
+    <p>
+		<a id="all_in_one_invite_codes_profile" href="#TB_inline?width=800&height=auto&inlineId=all_in_one_invite_codes_profile_modal" title="" class="thickbox button"><?php _e( 'Create Invite', 'all-in-one-invite-code' ) ?></a>
+	</p>
+    <div id="all_in_one_invite_codes_profile_modal" style="display:none;">
+		<div id="buddyforms_invite_wrap">
+            <?php do_shortcode('[all_in_one_invite_codes_create]'); ?>
+            <button id="" data-post_id="<?php echo $post_id ?>" href="#" class="button"><?php echo __( 'Sent Invite', 'buddyforms-collaborative-publishing' ) ?></button>
+		</div>
+    </div>
+    <?php
 	echo all_in_one_invite_codes_list_codes( $args );
 }
 
